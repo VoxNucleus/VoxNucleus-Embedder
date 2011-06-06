4f19330613ecb8a18e499c71dd1f0b7216ffc43e
Victor Kabdebon <admin@voxnucleus.fr>
2011-06-06 15:04:01 -0700

    v0.2 version

--- examples/vn_embedder.js
+++ examples/vn_embedder.js
@@ -16,14 +16,20 @@
    $.fn.vn_embedder= function(params){
      var default_params={
        verification:true,
-       container_class:'div',
+       container_class:'vn_vid_cont',
        trigger_class:'vn_emb',
        target_class:'vn_emb_target',
-       embedder_target:"",
+       embedder_target_id:"",
        vid_params:{
 	 hd:true,
 	 height:500,
 	 width:500
+       },
+       messages:{
+	 server_error:"Erreur serveur",
+	 error:"Erreur",
+	 not_found:"Non trouve",
+	 trigger_code:"Lancer video"
        },
        embedder_server:'/embedder-server',
        jsonp:false
@@ -34,110 +40,103 @@
 
      this.each(function(){
 
-       var vid_address=$(this).attr("href");
-       var to_embed=this;
-       var verif_opt={
-	 url:params.embedder_server+"/verification",
-	 type:"GET",
-	 data:{
-	   requesting:'ispresent',
-	   from:vid_address},
-       success:function(data, textStatus, jqXHR){
+		 var vid_address=$(this).attr("href");
+		 var to_embed=this;
+		 var verif_opt={
+		   url:params.embedder_server+"/verification",
+		   type:"GET",
+		   data:{
+		     requesting:'ispresent',
+		     from:vid_address},
+		   success:function(data, textStatus, jqXHR){
 
-	 if(data.success=="true"){
-	   if(data.present=="true"){
-	     //
-	     var old_code=$(to_embed).clone();
+		     if(data.success=="true"){
+		       if(data.present=="true"){
+			 var old_code=$(to_embed).clone();
+			 var new_dom= new_dom=document.createElement("div");
+			 new_dom.setAttribute("class","embedder_container");
+			 var new_dom_trig=document.createElement("div");
+			 new_dom_trig.setAttribute("class","trigger");
+			 new_dom_trig.innerHTML=params.messages.trigger_code;
+			 new_dom.appendChild(new_dom_trig);
+			 $(new_dom).append(old_code);
+			 $(to_embed).replaceWith(new_dom);
 
-	     var new_dom=document.createElement("div");
-	     new_dom.setAttribute("class","embedder_container");
-	     var new_dom_trig=document.createElement("div");
-	     new_dom_trig.setAttribute("class","trigger");
-	     new_dom_trig.innerHTML="Embed";
-	     new_dom.appendChild(new_dom_trig);
-	     $(new_dom).append(old_code);
-	     $(to_embed).replaceWith(new_dom);
-	     //on click
-	     $(new_dom_trig).click(function(){
-	       var options={
-		 url:params.embedder_server+"/request",
-		 type:"GET",
-		 data:{
-		   from:vid_address,
-		   width:params.width,
-		   height:params.height,
-		   hd:params.hd},
-		 success:function(data, textStatus, jqXHR){
-		   if(data.status=="found"){
-		     //Set the trigger to expended
-		     //$(trigger).attr("expended","expended");
-		     $(new_dom).append(
-		     data.code
-		   );
-		   }else if(data.status=="error"){
-		     $(parent).append("<span class=\"emb_error\">Erreur</span>"+data.explanation);
-		   }else if(data.status=="notfound"){
-		     $(parent).append("<span class=\"emb_notfound\">Non trouve</span>");
+
+			 //Here we verify if there is a object with specified id where we can
+			// if(embedder_target_id!="" &&
+			//    $("#"+embedder_target_id).length>0){
+			//   new_dom=$("#"+embedder_target_id);
+			// }
+
+			 //on click
+			 $(new_dom_trig)
+			   .click(function(){
+				    // Here we verify if there is a object with specified id where we can
+				    var where_to_insert=$(this).parent();
+				    if(params.embedder_target_id!="" &&
+				       $("#"+params.embedder_target_id).length>0){
+				      where_to_insert=$("#"+params.embedder_target_id);
+				    }
+
+				    var is_expended=$(this).attr("vn_expanded");
+				    if(is_expended=="true"){
+				      //Minimize & delete
+
+				      where_to_insert
+					.find("."+params.container_class)
+					.slideUp('slow',function(){
+						   $(this).remove();
+						   $(new_dom_trig).attr("vn_expanded","false");
+						 });
+				    }else{
+				      //Query normally
+				      var options={
+					url:params.embedder_server+"/request",
+					type:"GET",
+					data:{
+					  from:vid_address,
+					  width:params.width,
+					  height:params.height,
+					  hd:params.hd},
+					success:function(data, textStatus, jqXHR){
+					  if(data.status=="found"){
+					    //Set the trigger to expended
+					    var video_container = document.createElement("div");
+					    $(video_container).addClass(params.container_class);
+					    $(video_container).append(data.code);
+					    $(where_to_insert).append(video_container);
+					  }else if(data.status=="error"){
+					    $(where_to_insert).append("<span class=\" "+params.container_class+" emb_error\">"+params.messages.error+"</span>"+data.explanation);
+					  }else if(data.status=="notfound"){
+					    $(where_to_insert).append("<span class=\" "+params.container_class+" emb_notfound\">"+params.messages.not_found+"</span>");
+					  }
+					  $(new_dom_trig).attr("vn_expanded","true");
+					},
+					error:function(jqXHR, textStatus, errorThrown){
+					  $(where_to_insert).append("<span class=\""+params.target_class+" emb_error\">"+params.messages.server_error+"</span>");
+					},
+					dataType:"json"
+				      };
+				      $.ajax(options);
+				    }
+
+				  });
+
+		       }else{
+			 //Impossible to embed something
+		       }
+		     }else{
+		       //does not correspond to anything
+		     }
+
+		   },
+		   error:function(jqXHR, textStatus, errorThrown){
+		     //TODO : Problem occured here
 		   }
-		 },
-		 error:function(jqXHR, textStatus, errorThrown){
-		   $(parent).append("<span class=\"emb_error\">Erreur du serveur</span>");
-		 },
-		 dataType:"json"
-	       };
-	     $.ajax(options);
-
+		 };
+		 $.ajax(verif_opt);
 	       });
-
-	   }else{
-	     //Impossible to embed something
-	   }
-	 }else{
-	   //does not correspond to anything
-	 }
-
-       },
-       error:function(jqXHR, textStatus, errorThrown){
-	 //TODO : Problem occured here
-       }
-       };
-       $.ajax(verif_opt);
-/*
-     trigger=this;
-     parent=$(this).parent();
-     parent.find("."+params.trigger_class).click(function(){
-       var vid_address=$(parent).find("."+params.target_class).attr("href");
-       var options={
-	 url:params.embedder_server,
-	 type:"GET",
-	 data:{from:vid_address,
-	       width:params.width,
-	       height:params.height,
-	       hd:params.hd},
-	 success:function(data, textStatus, jqXHR){
-	   if(data.status=="found"){
-	     //Set the trigger to expended
-	     $(trigger).attr("expended","expended");
-	     $(parent).append(
-	       data.code
-	     );
-	   }else if(data.status=="error"){
-	     $(parent).append("<span class=\"emb_error\">Erreur</span>");
-	   }else if(data.status=="notfound"){
-	     $(parent).append("<span class=\"emb_notfound\">Non trouve</span>");
-	   }
-	 },
-	 error:function(jqXHR, textStatus, errorThrown){
-	   $(parent).append("<span class=\"emb_error\">Une erreur server s\'est produite</span>");
-	 },
-	 dataType:"json"
-
-       };
-       $.ajax(options);
-	       });*/
-
-
-       });
 
    };
  })(jQuery);
