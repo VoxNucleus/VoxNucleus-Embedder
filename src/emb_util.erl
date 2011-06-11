caf835f5fdd40fb16cf9078ea091c970bde2737f
Victor Kabdebon <admin@voxnucleus.fr>
2011-06-11 12:30:17 -0700

    Working towards v0.3, New compatibilities and new method (shortcode)

---
+++ src/emb_util.erl
@@ -0,0 +1,10 @@
+%%
+%% File : emb_util.erl
+%% Description : Utils used in embedder
+%% Author : Victor Kabdebon
+%% Date 06/11/2011
+
+-module(emb_util).
+-author("Victor Kabdebon").
+
+-export([string_to_tuple/1]).
